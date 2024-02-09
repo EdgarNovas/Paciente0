99ee3c18ca3f1629b5f4af1a8debf8c18970a84a
Edgar Novas <edgar.novas@enti.cat>
2024-02-09 10:03:37 +0100

    Unity Add
    First Unity Added

---
+++ Assets/Scripts/Enemy/ChasePlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float speed;
+    [SerializeField] float stoppingDistance;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = player.position - transform.position;
+        
+        if(Vector2.Distance(player.position, transform.position)> stoppingDistance )
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            //Attack
+            Debug.Log("Atacado shulo");
+        }
+        
+       
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+
+        Vector3 direction = player.position - transform.position;
+
+        
+            Gizmos.DrawRay(transform.position,direction);
+       
+    }
+
+}
