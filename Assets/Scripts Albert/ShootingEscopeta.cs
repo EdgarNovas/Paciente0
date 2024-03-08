a56c1ae455fb13bb24caeb4c51fd7085d1021531
AlbertMarch <albert.march@enti.cat>
2024-03-08 10:13:16 +0100

    Retoque escopeta

--- Assets/Scripts Albert/ShootingEscopeta.cs
+++ Assets/Scripts Albert/ShootingEscopeta.cs
@@ -6,8 +6,10 @@ public class ShootingEscopeta : MonoBehaviour
 {
     public Transform firePoint;
     public Transform firePoint2;
+    public Transform firePoint3;
     public GameObject bulletPrefab;
     public GameObject bulletPrefab2;
+    public GameObject bulletPrefab3;
 
     public float bulletForce = 20f;
 
@@ -24,9 +26,12 @@ public class ShootingEscopeta : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         GameObject bullet2 = Instantiate(bulletPrefab2, firePoint2.position, firePoint2.rotation);
+        GameObject bullet3 = Instantiate(bulletPrefab3, firePoint3.position, firePoint3.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up *  bulletForce, ForceMode2D.Impulse);
         rb2.AddForce(firePoint2.up *  bulletForce, ForceMode2D.Impulse);
+        rb3.AddForce(firePoint3.up *  bulletForce, ForceMode2D.Impulse);
     }
 }
