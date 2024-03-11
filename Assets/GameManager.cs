5696c2e252b8ef1284e28b9fda9c584029f46a34
Iker Fer <iker.fernandez@enti.cat>
2024-03-11 13:22:34 +0100

    Game Over scene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,8 +9,6 @@ public class GameManager : MonoBehaviour
 
     public int rescuedPeople = 0;
 
-    public string sceneToLoad;
-
     [field: SerializeField] public int life { get; private set; } = 100;
 
     private void Awake()
@@ -45,11 +43,6 @@ public class GameManager : MonoBehaviour
         {
             life = 4;
         }
-    }
-
-    public void OnRestart()
-    {
-        SceneManager.LoadScene(sceneToLoad);
     }
 }
 
