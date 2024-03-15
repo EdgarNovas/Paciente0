8d7bdfc708669940a1a199b2fbc8f49498c51552
JoanaMorenoC <juana.moreno@enti.cat>
2024-03-15 10:28:52 +0100

    Finish safe zone progress bar

--- Assets/Scripts/SafeZone.cs
+++ Assets/Scripts/SafeZone.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+using JetBrains.Annotations;
 
 public class SafeZone : MonoBehaviour
 {
+    public Image barProgress;
+    public Image barBackground;
+    float barYSpacing = 0.8f;
     bool playerInsideRange = false;
-    float secondsToStay = 2;
+    float secondsToStay = 2.0f;
     float startTime;
     float elapsedTime;
+    float barFilledPercentage = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +26,30 @@ public class SafeZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        barProgress.transform.position = this.gameObject.transform.position + new Vector3(0, barYSpacing, 0);
+        barBackground.transform.position = this.gameObject.transform.position + new Vector3(0, barYSpacing, 0);
+
         if (playerInsideRange && GameManager.Instance.GetRescuedPeople() == 3)
         {
+            barFilledPercentage = elapsedTime / secondsToStay;
+            barProgress.fillAmount = barFilledPercentage; 
             elapsedTime = Time.time - startTime;
             if (elapsedTime > secondsToStay)
             {
-                //SceneManager.LoadScene("Escena que sea");
+                // Cargar la escena que sea
+                SceneManager.LoadScene("SampleScene"); 
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && GameManager.Instance.GetRescuedPeople() == 3)
         {
             startTime = Time.time;
             playerInsideRange = true;
+            barProgress.enabled = true;
+            barBackground.enabled = true;
         }
     }
 
@@ -44,6 +58,8 @@ public class SafeZone : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")) 
         {
             playerInsideRange = false;
+            barProgress.enabled = false;
+            barBackground.enabled = false;
         }
     }
 }
